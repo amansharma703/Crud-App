ef10bed6902d25f2735337fbda9fc04b8f854758
Aman Sharma <aman703jk@gmail.com>
2022-08-13 18:31:52 +0530

    implemented crud api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const path = require("path");
 
-// const connectDB = require("./server/database/connection");
+const connectDB = require("./server/database/connection");
 
 const app = express();
 
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080;
 app.use(morgan("tiny"));
 
 // mongodb connection
-// connectDB();
+connectDB();
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended: true }));
